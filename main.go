cb3e53e6a2e0d597e53b196411d6669d553dca77
philipphombergersap <p.homberger@sap.com>
2025-05-09 13:47:51 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	fmt.Println("Welcome to the Go project!")
 	fmt.Println("Add some Stuff no Release!")
+	fmt.Println("Add some Stuff no Release!")
 	fmt.Println("Add next Release!")
 	fmt.Println("Release 2.0")
 }
