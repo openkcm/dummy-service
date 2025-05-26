67db4fd0ed45b9dfd1ce7756d0d8d1537aa6b935
Philipp Homberger <p.homberger@sap.com>
2025-05-26 08:35:12 +0200

    Create package.go

---
+++ pkg/package.go
@@ -0,0 +1,9 @@
+// Package pkg provides utility functions for the dummy-service-2 project.
+package pkg
+
+import "fmt"
+
+// Hello prints a greeting message from the pkg module.
+func Hello() {
+    fmt.Println("Hello from the pkg module!")
+}
